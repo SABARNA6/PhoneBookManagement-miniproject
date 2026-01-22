9937ebf8f1bc4c7d3a60d7980fb7899596a218e2
sabarna6 <sabarnakrishbalu@gmail.com>
2026-01-22 11:32:28 +0530

    updating viteconfig for render

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [    tailwindcss(),
-react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  server: {
+    host: true, // listen on all interfaces
+    port: Number(process.env.PORT) || 10000, // Use Render's PORT or fallback locally
+    allowedHosts: [
+      'phonebookmanagement-miniproject.onrender.com',
+      'localhost'
+    ]
+  }
 })
